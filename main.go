fc1610dd75d90344618dcbada45c5ad610ca5aec
adolfo-ab <adolfoab6@gmail.com>
2024-04-08 18:33:10 +0200

    Add Planet and PlanetType types

--- main.go
+++ main.go
@@ -5,6 +5,26 @@ import (
 	"net/http"
 )
 
+type Planet struct {
+	id               string
+	name             string
+	planetType       PlanetType
+	mass             float64
+	radius           float64
+	orbitalPeriod    float64
+	distanceFromStar float64
+}
+
+type PlanetType int
+
+const (
+	Undefined PlanetType = iota
+	Terrestrial
+	GasGiant
+	IceGiant
+	DwarfPlanet
+)
+
 type server struct{}
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
