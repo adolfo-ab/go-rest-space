6743009b24ccc9593d3be6200ffba38235c3f715
adolfo-ab <adolfoab6@gmail.com>
2024-04-12 08:09:32 +0200

    Add router.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,10 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"log"
 	"main/internal/domain/planet"
-	"main/internal/interfaces"
-	"net/http"
 )
 
 var Planets []planet.Planet
-
-func handleRequests() {
-	router := mux.NewRouter().StrictSlash(true)
-
-	router.HandleFunc("/", interfaces.HomePage)
-	router.HandleFunc("/planets", interfaces.ReturnAllPlanets)
-	router.HandleFunc("/planet", interfaces.CreateNewPlanet).Methods("POST")
-	router.HandleFunc("/planet/{id}", interfaces.ReturnSinglePlanet).Methods("GET")
-	router.HandleFunc("/planet/{id}", interfaces.DeletePlanet).Methods("DELETE")
-	router.HandleFunc("/planet/{id}", interfaces.UpdatePlanet).Methods("PUT")
-
-	log.Fatalln(http.ListenAndServe(":10000", router))
-}
 
 func main() {
 	handleRequests()
---
+++ internal/interfaces/router.go
@@ -0,0 +1,20 @@
+package interfaces
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func handleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", HomePage)
+	router.HandleFunc("/planets", ReturnAllPlanets)
+	router.HandleFunc("/planet", CreateNewPlanet).Methods("POST")
+	router.HandleFunc("/planet/{id}", ReturnSinglePlanet).Methods("GET")
+	router.HandleFunc("/planet/{id}", DeletePlanet).Methods("DELETE")
+	router.HandleFunc("/planet/{id}", UpdatePlanet).Methods("PUT")
+
+	log.Fatalln(http.ListenAndServe(":10000", router))
+}
