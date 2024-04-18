9ca19ee14e0beac077c6514e866d2d892de88cae
adolfo-ab <adolfoab6@gmail.com>
2024-04-18 08:04:12 +0200

    Add missing getters and setters for StarSystem aggregate

--- internal/domain/aggregates/star_system/star_system.go
+++ internal/domain/aggregates/star_system/star_system.go
@@ -5,13 +5,13 @@ import (
 	"github.com/google/uuid"
 	"main/internal/domain/entities/planet"
 	"main/internal/domain/entities/star"
+	"main/internal/domain/value_objects/star_type"
 )
 
 var (
 	ErrInvalidStar = errors.New("A star system needs a valid star")
 )
 
-// StarSystem is an aggregate that includes a Star (root entity) and Planets orbiting around it
 type StarSystem struct {
 	star    *star.Star
 	planets []*planet.Planet
@@ -22,23 +22,21 @@ func NewStarSystem(starName string) (StarSystem, error) {
 		return StarSystem{}, ErrInvalidStar
 	}
 
-	star := &star.Star{
+	s := &star.Star{
 		ID:   uuid.New(),
 		Name: starName,
 	}
 
 	return StarSystem{
-		star:    star,
+		star:    s,
 		planets: make([]*planet.Planet, 0),
 	}, nil
 }
 
-// Returns the ID of the star (root entity) of the star system aggregate
 func (s *StarSystem) GetID() uuid.UUID {
 	return s.star.ID
 }
 
-// Set the ID of the star (root entity) of the star system aggregate
 func (s *StarSystem) SetID(id uuid.UUID) {
 	if s.star == nil {
 		s.star = &star.Star{}
@@ -46,28 +44,104 @@ func (s *StarSystem) SetID(id uuid.UUID) {
 	s.star.ID = id
 }
 
-// Returns the Name of the star (root entity) of the star system aggregate
 func (s *StarSystem) GetName() string {
 	return s.star.Name
 }
 
-// Set the Name of the star (root entity) of the star system aggregate
-func (s *StarSystem) SetName(name string) {
+func (s *StarSystem) SetName(name string) error {
 	if s.star == nil {
-		s.star = &star.Star{}
+		return ErrInvalidStar
 	}
 	s.star.Name = name
+
+	return nil
 }
 
-// Returns the Mass of the star (root entity) of the star system aggregate
+func (s *StarSystem) GetStarType() star_type.StarType {
+	return s.star.StarType
+}
+
+func (s *StarSystem) SetStarType(st star_type.StarType) error {
+	if s.star == nil {
+		return ErrInvalidStar
+	}
+	s.star.StarType = st
+
+	return nil
+}
+
 func (s *StarSystem) GetMass() float64 {
 	return s.star.Mass
 }
 
-// Set the Mass of the star (root entity) of the star system aggregate
-func (s *StarSystem) SetMass(mass float64) {
+func (s *StarSystem) SetMass(mass float64) error {
 	if s.star == nil {
-		s.star = &star.Star{}
+		return ErrInvalidStar
 	}
 	s.star.Mass = mass
+
+	return nil
+}
+
+func (s *StarSystem) GetRadius() float64 {
+	return s.star.Radius
+}
+
+func (s *StarSystem) SetRadius(r float64) error {
+	if s.star == nil {
+		return ErrInvalidStar
+	}
+	s.star.Radius = r
+
+	return nil
+}
+
+func (s *StarSystem) GetTemperature() float64 {
+	return s.star.Temperature
+}
+
+func (s *StarSystem) SetTemperature(t float64) error {
+	if s.star == nil {
+		return ErrInvalidStar
+	}
+	s.star.Temperature = t
+
+	return nil
+}
+
+func (s *StarSystem) GetLuminosity() float64 {
+	return s.star.Luminosity
+}
+
+func (s *StarSystem) SetLuminosity(l float64) error {
+	if s.star == nil {
+		return ErrInvalidStar
+	}
+	s.star.Mass = l
+
+	return nil
+}
+
+func (s *StarSystem) GetMetallicity() float64 {
+	return s.star.Metallicity
+}
+
+func (s *StarSystem) SetMetallicity(m float64) error {
+	if s.star == nil {
+		return ErrInvalidStar
+	}
+	s.star.Mass = m
+
+	return nil
+}
+
+func (s *StarSystem) GetPlanets() []*planet.Planet { return s.planets }
+
+func (s *StarSystem) SetPlanets(p []*planet.Planet) error {
+	if s.star == nil {
+		return ErrInvalidStar
+	}
+	s.planets = p
+
+	return nil
 }
