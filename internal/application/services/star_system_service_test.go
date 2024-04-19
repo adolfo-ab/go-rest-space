8fb5991587a1a93f274827535ef641d4d27509f8
adolfo-ab <adolfoab6@gmail.com>
2024-04-19 08:26:18 +0200

    Add star system service test for create method

--- internal/application/services/star_system_service_test.go
+++ internal/application/services/star_system_service_test.go
@@ -1,1 +1,34 @@
 package services
+
+import (
+	"testing"
+)
+
+func TestStarSystemService_CreateStarSystem(t *testing.T) {
+	s, err := NewStarSystemService(
+		WithMemoryStarSystemRepository(),
+	)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	names := []string{"s1", "s2", "s3"}
+
+	for _, name := range names {
+		err = s.CreateStarSystem(name)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	starSystems, err := s.starSystems.GetAll()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(starSystems) != len(names) {
+		t.Errorf("Incorrect number of star systems in repository. Expected %v, got %v", len(names), len(starSystems))
+	}
+
+}
