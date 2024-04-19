8bf9f9bc4656ae3359ef54a67a2ae68019424746
adolfo-ab <adolfoab6@gmail.com>
2024-04-19 07:51:43 +0200

    Add service method to create star system

--- internal/application/services/star_system_service.go
+++ internal/application/services/star_system_service.go
@@ -1,15 +1,28 @@
 package services
 
-import "main/internal/domain/repositories/star_system_repository"
+import (
+	"main/internal/domain/aggregates/star_system"
+	"main/internal/domain/repositories/star_system_repository"
+)
 
 // TODO: Add config
 
 type StarSystemService struct {
-	starSystem star_system_repository.StarSystemRepository
+	starSystems star_system_repository.StarSystemRepository
 }
 
 func NewStarSystemService() *StarSystemService {
 	s := &StarSystemService{}
 
 	return s
+}
+
+func (s *StarSystemService) CreateStarSystem(name string) error {
+	starSystem, err := star_system.NewStarSystem(name)
+	if err != nil {
+		return err
+	}
+
+	err = s.starSystems.Add(starSystem)
+	return err
 }
